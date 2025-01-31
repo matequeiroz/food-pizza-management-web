3d99a8d3d5fae3980a65baef4cf416005fca088a
Mateus Queiroz <mateusqueirozdev@gmailcom>
2025-01-31 11:50:40 -0300

    chore: add eslint and prettier config in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
     <>
-      <button className="h-12 w-full bg-blue-500 border-r-4">Entrar</button>
+      <button className="h-12 w-full border-r-4 bg-blue-500">Entrar</button>
       <Button>Click me</Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
